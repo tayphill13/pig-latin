8d0d72f986ce2916be094de086354bd4dd7abb2e
William Pratt <willpratt78@gmail.com>
2020-04-01 15:15:38 -0700

    ask our function to search for vowels within a string and return true if vowels are found

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,26 @@
 // // business logic
 
-// const regex = /[a-z]/g;
-var input = "#word";
-
-// if ("#word" === input.match(/[a-z]/g))
-//   return true;
-// console.log(input)
-
-
-console.log(input)
-// example
-// var userInput = "Ben works at Epicodus.";
-// var userInput2 = "!@#@!#";
-
-// if (["e", "n", "w", "o", "r", "k", "s", "a", "t", "p", "i", "c", "o", "d", "u", "s"]){
-//   console.log(userInput.match(/[a-z]/g))
-// }
-
-// if ([]){
-//   console.log("input 2  good !")
-// }
-
 // Array(InputString)
   // turn the input string into an array
   // loop
   // is this a vowel? branching with .match()
 
+var vowel = ['a','e','i','o','u']
+
 var characters = function(letters) {
   if (letters.match(/[a-z]/g)){
-    console.log("test")
     return true;
   } else {
     return false;
   };
-  // break the sentenece down into an array of words
 };
-console.log(input.match(/[a-z]/g));
+
+  var letterType =function(kind) {
+  if (characters === true && letters === vowel)
+    return vowels;
+  };
+  // break the sentenece down into an array of words
+console.log(vowel);
 
 
 // if (letter === 'a' || letter === 'b')
