27461b6f2bd8d0bea02a77ec7a00f07806978be4
William Pratt <willpratt78@gmail.com>
2020-04-01 11:53:42 -0700

    continue debugging process as now all input is returned as false

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
 // business logic
-var letters = includes("q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m");
-console.log(letters);
+
+var letter =['a-z'];
+var capital=['A-Z'];
+
+// Array(InputString)
+  // turn the input string into an array
+  // loop
+  // is this a vowel? branching with .match()
+
+var characters = function(letters) {
+  if ("#word" === letter){
+    return true;
+  } else{
+    return false;
+  };
+};
+console.log(characters);
 
 
+// if (letter === 'a' || letter === 'b')
 
 //user interface logic
 
 $(document).ready(function() {
   $("form#pig").submit(function(event) {
     event.preventDefault();
+    var letters = $("input#word").val();
+    var result = characters(letters);
+    $('#result').text(result);
   })
 })
